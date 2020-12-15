80aecedc6153310ef44bde2c151649b5411c537c
Kasey Kirkham <kasey@upbound.io>
2020-12-14 22:11:52 -0600

    adding helper methods for code generation // TODO better place for these?

--- pkg/plugin/compare.go
+++ pkg/plugin/compare.go
@@ -11,4 +11,84 @@ type MergeDescription struct {
 	StatusUpdated       bool
 	AnnotationsUpdated  bool
 	NeedsProviderUpdate bool
+	AnyFieldUpdated     bool
+}
+
+func CompareInt64Slices(a, b []int64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	lookup := make(map[int64]struct{})
+	for _, x := range a {
+		lookup[x] = struct{}{}
+	}
+	for _, x := range b {
+		if _, ok := lookup[x]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func CompareStringSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	lookup := make(map[string]struct{})
+	for _, x := range a {
+		lookup[x] = struct{}{}
+	}
+	for _, x := range b {
+		if _, ok := lookup[x]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func CompareMapString(a, b map[string]string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for key, val := range a {
+		bv, ok := b[key]
+		if !ok || bv != val {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CompareMapInt64(a, b map[string]int64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for key, val := range a {
+		bv, ok := b[key]
+		if !ok || bv != val {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CompareMapBool(a, b map[string]bool) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for key, val := range a {
+		bv, ok := b[key]
+		if !ok || bv != val {
+			return false
+		}
+	}
+
+	return true
 }
